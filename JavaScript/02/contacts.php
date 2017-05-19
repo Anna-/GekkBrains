608fea163309c901f2ac5cabe00c8ddb6aa35d0f
Anna- <joannna@yandex.ru>
2017-05-19 18:29:22 +0300

    transformed to 'class'-style (?)

--- JavaScript/02/contacts.php
+++ JavaScript/02/contacts.php
@@ -13,39 +13,39 @@
 		<h2>Напишите нам</h2>
         <div id="message" class="error_text"></div>
         <form action="" method="post" name="feedback_form" id="feedback_form">
-        	<label for="input_name">Имя:</label>
-            <input type="text" id="input_username" name="username" placeholder="Имя" />
+        	<label for="username">Имя:</label>
+            <input type="text" id="username" name="username" placeholder="Имя" />
             <div id="username_message" class="error_text"></div>
 
-        	<label for="input_password">Пароль:</label>
-            <input type="password" id="input_password" name="password" placeholder="Пароль"/>
+        	<label for="password">Пароль:</label>
+            <input type="password" id="password" name="password" placeholder="Пароль"/>
             <div id="password_message" class="error_text"></div>
 
-            <label for="input_email">Email:</label>
-            <input type="email" id="input_email" name="email" placeholder="Email"/>
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" placeholder="Email"/>
             <div id="email_message" class="error_text"></div>
 
-            <label for="input_gender">Пол:</label>
-            <select id="input_gender" name="gender" >
+            <label for="gender">Пол:</label>
+            <select id="gender" name="gender" >
                 <option value="f">Женский</option>
                 <option value="m">Мужской</option>
             </select>
             <div id="gender_message" class="error_text"></div>
 
-            <label for="input_birth">День рождения:</label>
-            <input type="date" name="birth" id="input_birth">
+            <label for="birth">День рождения:</label>
+            <input type="date" name="birth" id="birth">
             <div id="birth_message" class="error_text"></div>
 
-            <label for="input_credit_card">Кредитная карта:</label>
-            <input type="text" id="input_credit_card" name="credit_card" placeholder="9999999-9999-999999-999""/>
+            <label for="credit_card">Кредитная карта:</label>
+            <input type="text" id="credit_card" name="credit_card" placeholder="9999999-9999-999999-999""/>
             <div id="credit_card_message" class="error_text"></div>
             <!-- type="tel" required="required" pattern="[0-9]{7}-[0-9]{4}-[0-9]{6}-[0-9]{3} -->
 
-            <label for="input_bio">О себе:</label>
-            <textarea name="bio" id="input_bio" placeholder="Напишите о себе"></textarea>
+            <label for="bio">О себе:</label>
+            <textarea name="bio" id="bio" placeholder="Напишите о себе"></textarea>
             <div id="bio_message" class="error_text"></div>
 
-            <input id="submit_button" type="submit" value="Проверить" name="submit" onclick="validate(); return false;" />
+            <input id="submit_button" type="submit" value="Проверить" name="submit" />
             <div class="clearfix"></div>
         </form>
     </div>
--- JavaScript/02/js/task_02.js
+++ JavaScript/02/js/task_02.js
@@ -3,94 +3,106 @@
 
 */
 
-var FIELDS = [
-	"Username",
-	"Password",
-	"Email",
-	"Gender",
-	"Credit Card",
-	"Bio",
-	"Birth"
-	];
+function FeedbackForm () {
+	var self = this;
+	var FIELDS = [
+		"Username",
+		"Password",
+		"Email",
+		"Gender",
+		"Credit Card",
+		"Bio",
+		"Birth"
+		];
 
-function toFormField (fieldName) {
-	return fieldName.toLowerCase().replace(" ", "_");
-}
-
-function submit() {
-	window.location.href = "contacts.php";
-}
-
-function validate () {
-	var xhr = new XMLHttpRequest();
-	xhr.open("POST", "validator.php", true);
-	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
-
-	xhr.timeout = 15000;
-	xhr.ontimeout = function () {
-		console.log("Время ожидания ответа превышено");
+	this.init = function (submitButtonId) {
+		document.getElementById(submitButtonId).onclick = function () {
+			self.validate();
+			return false;
+		};
 	}
 
-	xhr.onreadystatechange = function () {
-		console.log(xhr.readyState, xhr.status);
-		if (xhr.readyState == 4 && xhr.status == 200) {
-			parseResponse(xhr.responseText);
+	this.submit = function () {
+		window.location.href = "contacts.php";
+	}
+
+	this.validate = function () {
+		var xhr = new XMLHttpRequest();
+		xhr.open("POST", "validator.php", true);
+		xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
+
+		xhr.timeout = 15000;
+		xhr.ontimeout = function () {
+			console.log("Время ожидания ответа превышено");
 		}
-	}
 
-	var formData = getFormData();
-	console.log(formData);
-	xhr.send(formData);
-}
-
-function getFormData() {
-	var res = "";
-
-	for (var i in FIELDS) {
-		var field = toFormField(FIELDS[i]);
-		if (res != "") {
-			res += "&";
+		xhr.onreadystatechange = function () {
+			console.log(xhr.readyState, xhr.status);
+			if (xhr.readyState == 4 && xhr.status == 200) {
+				parseResponse(xhr.responseText);
+			}
 		}
-		res += (field + "=" + document.getElementById("input_" + field).value);
+
+		var formData = getFormData();
+		console.log(formData);
+		xhr.send(formData);
 	}
-	return res;
-}
 
-function parseResponse(text) {
-	var response = JSON.parse(text);
-
-	var result = response['result'];
-	if (result) {
-		console.log("Форму можно отправлять");
-		submit();
-	} else {
-		console.log("Некоторые поля заполнены некорректно");
-		document.getElementById('message').innerText = "Некоторые поля заполнены некорректно";
-		displayErrors(response['error']);
+	function toFormField (fieldName) {
+		return fieldName.toLowerCase().replace(" ", "_");
 	}
-}
 
-function displayErrors(errors) {
-	console.log(errors);
-	for (var i in FIELDS) {
-		var field = FIELDS[i];
-		var formField = toFormField(field);
+	function getFormData() {
+		var res = "";
 
-		if (errors[field]) {
-			showError(formField, errors[field]);
+		for (var i in FIELDS) {
+			var field = toFormField(FIELDS[i]);
+			if (res != "") {
+				res += "&";
+			}
+			res += (field + "=" + document.getElementById(field).value);
+		}
+		return res;
+	}
+
+	function parseResponse(text) {
+		var response = JSON.parse(text);
+
+		var result = response['result'];
+		if (result) {
+			console.log("Форму можно отправлять");
+			self.submit();
 		} else {
-			removeError(formField);
+			console.log("Некоторые поля заполнены некорректно");
+			document.getElementById('message').innerText = "Некоторые поля заполнены некорректно";
+			displayErrors(response['error']);
 		}
+	}
+
+	function displayErrors(errors) {
+		console.log(errors);
+		for (var i in FIELDS) {
+			var field = FIELDS[i];
+			var formField = toFormField(field);
+
+			if (errors[field]) {
+				showError(formField, errors[field]);
+			} else {
+				removeError(formField);
+			}
+		}
+	}
+
+	function showError(fieldName, textError) {
+		document.getElementById(fieldName + "_message").innerText = textError;
+		document.getElementById(fieldName).classname += "error";
+	}
+
+	function removeError(fieldName) {
+		document.getElementById(fieldName + "_message").innerText = "";
+		document.getElementById(fieldName).classList.remove("error");
 	}
 }
 
-function showError(fieldName, textError) {
-	document.getElementById(fieldName + "_message").innerText = textError;
-	document.getElementById("input_" + fieldName).classname += "error";
-}
-
-function removeError(fieldName) {
-	document.getElementById(fieldName + "_message").innerText = "";
-	document.getElementById("input_" + fieldName).classList.remove('error');
-}
-
+var form = new FeedbackForm();
+form.init("submit_button");
